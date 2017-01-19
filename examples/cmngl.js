54a4d50284c923c2703b409e0e84390ce013386c
Gary Hsu <garyyuya@hotmail.com>
2017-01-19 13:35:20 -0800

    Adding ngl mouse event function (show contact when clicked on ngl residue)

--- examples/cmngl.js
+++ examples/cmngl.js
@@ -4,14 +4,53 @@ function nglClass(name, vp){
 	var structurecomp;
 	var viewport = vp;
 
+
 	function loadngl(){
 		stage = new NGL.Stage( viewport );
 
+		//5sx3
 	    stage.loadFile( "rcsb://1smt.mmtf", { defaultRepresentation: true } ).then( function( o ){
 
 	    	structurecomp = o;
 
-		});		
+		});	
+
+
+
+
+	    //mouseevent for ngl
+	    var clickedresno;
+		stage.signals.clicked.add( 
+			function( pickingData )
+			{ 	
+				if(pickingData.atom){
+					console.log(pickingData.atom.resno);
+					clickedresno = pickingData.atom.resno;
+
+					d3.selectAll("rect").style("fill", "steelblue");
+					//i = id of the rect
+					//d = data insert into rect 
+					d3.selectAll("rect").each(function(d,i){
+
+						if(d[0] == clickedresno || d[1] == clickedresno){
+
+							d3.select(this).style("fill","orange");
+
+						}
+					});
+
+				}
+
+				else{
+					d3.selectAll("rect").style("fill", "steelblue");
+				}
+			} 
+		);
+
+
+
+
+
 	};
 
 	this.loadngl = function(){
@@ -20,6 +59,10 @@ function nglClass(name, vp){
 
 	this.getStructureComp = function(){
 		return structurecomp;
+	}
+
+	this.getClickedResno = function(){
+		return clickedresno;
 	}
 
 }
