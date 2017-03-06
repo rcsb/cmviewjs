43c61b54fbb596938e1cc0b98e3c3eb9ca497807
Gary Hsu <garyyuya@hotmail.com>
2017-03-06 15:13:38 -0800

    Adding distance and licorice representation using mouse drag(brush)

--- examples/cmsvg.js
+++ examples/cmsvg.js
@@ -7,6 +7,8 @@ function cmSvg(name, ngl1, pdburl){
 	var res1 = ngl1.res1();
 	var res2 = ngl1.res2();
 	var svgdata = ngl1.svgdata();
+	var brushtag = 0;
+
 
 
 	function initResData(size, residue1, residue2, residuerectdata){
@@ -36,6 +38,10 @@ function cmSvg(name, ngl1, pdburl){
 		var repr1;
 
 		function mouseover(p){
+			if (brushtag === 1){
+				//console.log("HI");
+				return;
+			} 
 			var tempx = p[0];
 			var tempy = p[1];
 
@@ -43,19 +49,24 @@ function cmSvg(name, ngl1, pdburl){
 			coordx = tempx;
 			coordy = tempy;
 
-			//showing distance on ngl when mouse over in contact map
+			//showing distance and sidechain on ngl when mouse over in contact map
 			if(mousetag === 1){
 				ngl.getStructureComp().removeRepresentation(repr);
-				//ngl.getStructureComp().removeRepresentation(repr1);
+				ngl.getStructureComp().removeRepresentation(repr1);
 				
 			}
 			var inputx = coordx + ".CA";
 			var inputy = coordy + ".CA";
 
+			var sidechainx = coordx + ":A";
+			var sidechainy = coordy + ":A";
+
 			var atomPair = [[inputx,inputy]];
-			var atomPair1 = [inputx];
-			repr = ngl.getStructureComp().addRepresentation( "distance", { atomPair: atomPair } );
-			//repr1 = ngl.getStructureComp().addRepresentation("licorice");
+			
+			var sidechainselec = "("+sidechainx +" or "+ sidechainy +")" ;
+			//+ " and (sidechainAttached)"
+			repr = ngl.getStructureComp().addRepresentation( "distance", { atomPair: atomPair });
+			repr1 = ngl.getStructureComp().addRepresentation("licorice", { sele: sidechainselec});
 			mousetag = 1;
 
 
@@ -82,12 +93,12 @@ function cmSvg(name, ngl1, pdburl){
 			.style("opacity", 0);
 
 			//change the opacity back to 1 when mouseout
-			d3.select(this).style("opacity", 1);			       
+			d3.select(this).style("opacity", 1);	
+
+			//clear ngl
+			ngl.getStructureComp().removeRepresentation(repr);	
+			ngl.getStructureComp().removeRepresentation(repr1);		       
 		}
-
-				
-
-
 
 		
 		//drawing many background gray rects
@@ -177,16 +188,119 @@ function cmSvg(name, ngl1, pdburl){
 		    		.scaleExtent([1, zoomfactor])
 		    		.translateExtent([[0, 0], [svgsize, svgsize]])
 		    		.on("zoom", zoomed);
-		
-		svgContainer.call(zoom);			
+
+		//svgContainer.call(zoom);			
 
 		function zoomed() {
+			if (brushtag === 1){
+				//console.log("HI");
+				return;
+			}
 			rects1blue.attr("transform", d3.event.transform);
 			classlinex.attr("transform", d3.event.transform);
 			classliney.attr("transform", d3.event.transform);
 			bAxisGroup.call(bAxis.scale(d3.event.transform.rescaleX(axisScale)));
 			rAxisGroup.call(rAxis.scale(d3.event.transform.rescaleY(axisScale)));
+		
 		}
+
+
+
+
+
+		//brush function 
+		var brush = d3.brush()
+					.on("start brush", brushstart)
+					.on("end", brushend);
+		var svgBrush = svgContainer.append("g")
+		    .attr("class", "brush")
+		    .call(brush);	
+
+		var repr2;
+		var repr3;
+		var atomPair1 = [];
+		var sidechainselec1 = "(";
+		function brushstart(){
+
+			brushtag = 1;
+
+			/*if (d3.event.sourceEvent && d3.event.sourceEvent.type === "zoom"){
+				console.log("HI");
+				return;
+			}*/
+
+			var s = d3.event.selection;
+			var xstart, xend, ystart, yend;
+			
+
+			//clear everything
+			d3.selectAll(".blue").selectAll("rect").style("fill", "steelblue");
+			ngl.getStructureComp().removeRepresentation(repr2);	
+			ngl.getStructureComp().removeRepresentation(repr3);
+			atomPair1 = [];
+			sidechainselec1 = "";
+
+
+			xstart = s[0][0];
+			xend = s[1][0];
+			ystart = s[0][1];
+			yend = s[1][1];
+
+			/*
+			console.log("xstart: " + xstart);
+			console.log("ystart: " + ystart);
+			console.log("xend: " + xend);
+			console.log("yend: " + yend);*/
+
+			//saving representation info while dragging
+			d3.selectAll(".blue").selectAll("rect").each(function(d,i){
+				
+				if(d[0]*unit >= xstart && d[0]*unit <= xend && d[1]*unit >= ystart && d[1]*unit <= yend){
+
+					d3.select(this).style("fill","PaleVioletRed");
+
+					var inputx = d[0] + ".CA";
+					var inputy = d[1] + ".CA";
+					atomPair1.push([inputx,inputy]);
+
+					var sidechainx = d[0] + ":A";
+					var sidechainy = d[1] + ":A";
+					sidechainselec1 = sidechainselec1 + sidechainx + " or " + sidechainy + " or ";
+				}
+			});
+					
+		}
+
+		function brushend(){
+			if(!d3.event.selection){
+				//clear everything when clear on gray rect
+				d3.selectAll(".blue").selectAll("rect").style("fill", "steelblue");
+				atomPair1 = [];
+				sidechainselec1 = "";
+				ngl.getStructureComp().removeRepresentation(repr2);	
+				ngl.getStructureComp().removeRepresentation(repr3);
+
+				brushtag = 0;
+			}
+
+			else{
+				//adding representation in ngl after mouse release
+				//ngl.getStructureComp().removeRepresentation(repr2);
+				//ngl.getStructureComp().removeRepresentation(repr3);
+				sidechainselec1 = sidechainselec1.substring(0, sidechainselec1.length-3);
+				sidechainselec1 = sidechainselec1 + ")";
+				//console.log(sidechainselec1);
+				if(sidechainselec1 !== ")"){
+					repr2 = ngl.getStructureComp().addRepresentation( "distance", { atomPair: atomPair1 });
+					repr3 = ngl.getStructureComp().addRepresentation("licorice", { sele: sidechainselec1});
+				}
+			}
+		}
+
+
+
+
+
 
 
 		//creating scale for axis 
@@ -300,4 +414,5 @@ function cmSvg(name, ngl1, pdburl){
 	this.getresiduesize = function(){
 		return residuesize;
 	}
+
 }
